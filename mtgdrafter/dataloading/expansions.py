dc06b4ec4a9804261a7ce8db502b81f93cbf1459
Gabe Gustafson <60358481+GabeGustafson@users.noreply.github.com>
2025-02-01 10:38:24 -0600

    Adding name to idx mapping

--- mtgdrafter/dataloading/expansions.py
+++ mtgdrafter/dataloading/expansions.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
+BASICS = ["plains", "island", "swamp", "mountain", "forest"]
+
 # Stores the raw card data and features for a given expansion
 class Expansion:
     def __init__(self, cards: pd.DataFrame):
         self.cards = cards
         self.num_cards = cards.shape[0]
+
+        card_names = BASICS + [c.name for c in self.cards[0]]
+        self.card_name_to_id = {n: i for i, n in enumerate(card_names)}
--- mtgdrafter/scripts/run_downloader.py
+++ mtgdrafter/scripts/run_downloader.py
@@ -1,8 +1,11 @@
 from mtgdrafter.dataloading.scryfall_downloader import *
+from mtgdrafter.dataloading.expansions import Expansion
 import pandas as pd
 
 if __name__ == "__main__":
     cards = get_scryfall_cards(["set=fdn", "is:booster"])
 
-    print(cards)
-    print(cards.shape)
+    # print(cards)
+
+    exp = Expansion(cards)
+    print(exp.card_name_to_id)
